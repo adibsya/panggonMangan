a3965da1410bf58a1b7ba2e0667761471b758462
ahmad adib <ahmadadibsyaifulloh@gmail.com>
2025-07-10 19:28:42 +0700

    commit pertama

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+      ducimus eligendi numquam beatae nesciunt? Perspiciatis, mollitia
+      consequuntur nihil reprehenderit assumenda laudantium saepe, cupiditate
+      quibusdam illum iusto nisi ab dolorem fuga!
+    </div>
+  );
+};
+
+export default App;
